52608206faa65449114a68ecc8cfce7757868f93
BryanNun <nuninger.bryan@gmail.com>
2019-07-10 15:58:25 +0200

    Added Player.java

---
+++ src/main/java/com/wildrunners/wildrunners/entities/Player.java
@@ -0,0 +1,31 @@
+package com.wildrunners.wildrunners.entities;
+
+public class Player {
+
+    private String player1;
+    private String player2;
+
+    public Player(String player1, String player2) {
+        this.setPlayer1(player1);
+        this.setPlayer2(player2);
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+
+
+}

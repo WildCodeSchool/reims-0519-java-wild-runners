12c5ea5c962f136cd09ac428076cef821c029051
Stephane Thomin <thomin.stephane@gmail.com>
2019-07-10 11:47:55 +0200

    spring init

---
+++ src/main/java/com/wildrunners/wildrunners/WildrunnersApplication.java
@@ -0,0 +1,13 @@
+package com.wildrunners.wildrunners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildrunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildrunnersApplication.class, args);
+	}
+
+}

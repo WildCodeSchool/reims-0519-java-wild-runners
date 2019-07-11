0e7712ae6db18b286a6f54948a662086fcfeb438
Stephane Thomin <thomin.stephane@gmail.com>
2019-07-11 11:04:28 +0200

    changes for css grid

--- src/main/java/com/wildrunners/wildrunners/controllers/GameController.java
+++ src/main/java/com/wildrunners/wildrunners/controllers/GameController.java
@@ -1,5 +1,7 @@
 package com.wildrunners.wildrunners.controllers;
 
+import com.wildrunners.wildrunners.entities.Cell;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-class GameController {
+public class GameController {
     @GetMapping("/")
     public String homepage() {
         return "homepage";
     }
 
     @GetMapping("/game")
-    public String game() {
+    public String game(Model model) {
+        Cell[][] grid = {
+            {new Cell("l1c1", false), new Cell("l2c1", false), new Cell("l3c1", false), new Cell("l4c1", false)},
+            {new Cell("l1c2", false), new Cell("l2c2", false), new Cell("l3c2", true), new Cell("l4c2", false)},
+            {new Cell("l1c3", true), new Cell("l2c3", false), new Cell("l3c3", false), new Cell("l4c3", false)}
+        };
+        model.addAttribute("grid", grid);
         return "game";
     }
 

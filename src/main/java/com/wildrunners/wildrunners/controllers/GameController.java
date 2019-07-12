854f94ea00968e41ae9a15ae1bb8074e1a1d914d
Stephane Thomin <thomin.stephane@gmail.com>
2019-07-12 02:48:39 +0200

    moving player

--- src/main/java/com/wildrunners/wildrunners/controllers/GameController.java
+++ src/main/java/com/wildrunners/wildrunners/controllers/GameController.java
@@ -44,7 +44,7 @@ public class GameController {
     @GetMapping("/game")
     public String game(Model model, HttpSession session) {
         if(session.getAttribute("player1") == null) {
-            session.setAttribute("player1", new Player(1, "Bryan-Stéphane-Vincent", 1, 1));
+            session.setAttribute("player1", new Player(1, "Bryan-Stéphane-Vincent", 0, 0));
         }
 
         model.addAttribute("currentPlayer", session.getAttribute("player1"));
@@ -71,14 +71,37 @@ public class GameController {
             for(int i = 0; i < moveSize; i++) {
                 player.setY(player.getY() - 1);
                 // est-ce que je suis sur un obstacle ?
+                if(this.grid[player.getY()][player.getX()].isObstacle()){
+                    player.setY(Math.min(player.getY() + 2, 3));
+                }
             }
         }
         if(move.equals("down")) {
-            player.setY(player.getY() + moveSize);
+            for(int i = 0; i < moveSize; i++) {
+                player.setY(player.getY() + 1);
+                if(this.grid[player.getY()][player.getX()].isObstacle()){
+                    player.setY(Math.max(player.getY() - 2, 0));
+                }
+            }
         }
         if(move.equals("right")) {
-            player.setX(player.getX() + moveSize);
+            for(int i = 0; i < moveSize; i++) {
+                player.setX(player.getX() + 1);
+                if(this.grid[player.getY()][player.getX()].isObstacle()){
+                    player.setX(Math.max(player.getX() - 2, 0));
+                }
+            }
         }
+
+        if(move.equals("left")) {
+            for(int i = 0; i < moveSize; i++) {
+                player.setX(player.getX() - 1);
+                if(this.grid[player.getY()][player.getX()].isObstacle()){
+                    player.setX(Math.min(player.getX() + 2, 13));
+                }
+            }
+        }
+
 
         if(won) {
             return "redirect:/win";
